5837ce1cebe29a63f1e772ba1406f7e0df46c629
Wonkeun No <59734889+Genne23v@users.noreply.github.com>
2022-02-28 23:05:57 -0500

    Decorator pattern added

---
+++ Decorator/Pizza.java
@@ -0,0 +1,14 @@
+package Decorator;
+
+public interface Pizza {
+//	public abstract void setDescription(String newDescription);
+//	public abstract String getDescription();
+//	
+//	public abstract void setCost(double newCost);
+//	public abstract double getCost();
+//	
+//	public abstract boolean hasFontina();
+	
+	public String getDescription();
+	public double getCost();
+}
---
+++ Decorator/ThreeCheesePizza.java
@@ -0,0 +1,45 @@
+package Decorator;
+
+public class ThreeCheesePizza implements Pizza {
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getCost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+//	@Override
+//	public void setDescription(String newDescription) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public String getDescription() {
+//		return "Mozzarella, Fontina, Parmesan, Cheese Pizza";
+//	}
+//
+//	@Override
+//	public void setCost(double newCost) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public double getCost() {
+//		return 10.0;
+//	}
+//
+//	@Override
+//	public boolean hasFontina() {
+//		// TODO Auto-generated method stub
+//		return false;
+//	}
+	
+}
